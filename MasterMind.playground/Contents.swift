27a4d3fb82e26b4ac577b01ae67fde7692027258
Soumya Saxena <soumya@Soumyas-MacBook-Pro.local>
2017-07-05 20:14:58 -0700

    Add MasterMind playground

---
+++ MasterMind.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+class MasterMind {
+    
+    func playGame() {
+        welcome()
+        choiceOfLevel()
+        
+//        while <#condition#> {
+//            
+//        }
+    }
+    
+    func welcome() {
+       print("Welcome to MasterMind, Soumya's swift version")
+    }
+    
+    func choiceOfLevel() {
+        
+    }
+    
+    func randomGeneratedCode(){
+        
+    }
+    
+    func userInputCode() {
+        
+    }
+    
+    func compare2Codes() {
+        
+    }
+    
+    func resultMessage(){
+    }
+}
